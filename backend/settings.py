86e01fec06fcc23c5bbaf648e47d3d590faf1299
Mukmilatu <muqmilat@gmail.com>
2023-04-18 22:16:46 +0100

    done

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'django_twilio',
+
     # APPLICATION APPS
     'account.apps.AccountConfig',
     'service.apps.ServiceConfig',
