34315a3c6ab0b92f47c41b7c94481de2088b8000
Mukmilatu <muqmilat@gmail.com>
2023-04-18 21:18:34 +0100

    Added production database

--- backend/settings.py
+++ backend/settings.py
@@ -120,9 +120,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "TP3k5SotenL7KDZSUa5f",
+        "HOST": "containers-us-west-7.railway.app",
+        "PORT": "7901",
     }
 }
 
